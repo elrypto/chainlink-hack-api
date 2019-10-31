b4aef0e0bf45cec014a8a58249f6c630b107a629
lrypto <lloydrebello@gmail.com>
2019-10-31 17:30:45 -0400

    added fetch from loom count

--- app/routes/routesNumber.js
+++ app/routes/routesNumber.js
@@ -1,4 +1,28 @@
-const { CryptoUtils, Client, LoomProvider, LocalAddress } = require("loom-js");
+
+ module.exports = function(app, db) {
+   app.get('/numberOfSkills', async(req, res) => {
+
+    let loomObj = await createLoomObj();
+
+    let skillsCount = await loomObj.instance.methods.numberOfSkills().call();
+
+
+    console.log(loomObj);
+
+     res.send(
+      JSON.stringify(
+        {
+          message: skillsCount,
+        }
+      )
+     )
+   })
+ }
+
+
+
+
+ const { CryptoUtils, Client, LoomProvider, LocalAddress } = require("loom-js");
 const UpikoContractJson = require('./UpikoApp.json');
 const Web3 = require('web3');
 
@@ -30,74 +54,60 @@ const Loom = {
 };
 
 
- module.exports = function(app, db) {
-   app.get('/numberOfSkills', async(req, res) => {
 
-    Loom.contract = LOOM_CONTRACT;
-    await createClient();
+async function createLoomObj() {
+  Loom.contract = LOOM_CONTRACT;
+  await createClient();
 
-    Loom.currentUserAddress = LocalAddress.fromPublicKey(
-      Loom.publicKey
-    ).toString();
+  Loom.currentUserAddress = LocalAddress.fromPublicKey(
+    Loom.publicKey
+  ).toString();
 
-    Loom.web3 = new Web3(new LoomProvider(Loom.client, Loom.privateKey));
-    await createContractInstance();
+  Loom.web3 = new Web3(new LoomProvider(Loom.client, Loom.privateKey));
+  await createContractInstance();
 
-    console.log(Loom);
-
-     res.send(
-      JSON.stringify(
-        {
-          message: '890',
-        }
-      )
-     )
-   })
- }
-
-
- async function testCall() {
-   console.log('k, good');
- }
-
- async function createClient() {
-  Loom.privateKey = CryptoUtils.generatePrivateKey();
-  Loom.publicKey = CryptoUtils.publicKeyFromPrivateKey(Loom.privateKey);
-  Loom.client = new Client(
-    Loom.connectionInfo.networkId,
-    Loom.connectionInfo.writeUrl,
-    Loom.connectionInfo.readUrl
-  );
-
-  Loom.client.on("error", msg => {
-    console.error("Error on connect to client", msg);
-    console.warn("Please verify if loom command is running");
-  });
+  return Loom;
 }
 
- async function createContractInstance() {
-  const networkId = await getCurrentNetwork();
-  Loom.currentNetwork = Loom.contract.networks[networkId];
-  //console.log("network:", Loom.currentNetwork);
+async function createClient() {
+Loom.privateKey = CryptoUtils.generatePrivateKey();
+Loom.publicKey = CryptoUtils.publicKeyFromPrivateKey(Loom.privateKey);
+Loom.client = new Client(
+  Loom.connectionInfo.networkId,
+  Loom.connectionInfo.writeUrl,
+  Loom.connectionInfo.readUrl
+);
 
-  if (!Loom.currentNetwork) {
-    console.error(
-      "not a valid network: , network id was:",
-      Loom.currentNetwork,
-      networkId
-    );
-    throw Error("Contract not deployed on DAppChain (network id error)");
+Loom.client.on("error", msg => {
+  console.error("Error on connect to client", msg);
+  console.warn("Please verify if loom command is running");
+});
+}
+
+async function createContractInstance() {
+const networkId = await getCurrentNetwork();
+Loom.currentNetwork = Loom.contract.networks[networkId];
+//console.log("network:", Loom.currentNetwork);
+
+if (!Loom.currentNetwork) {
+  console.error(
+    "not a valid network: , network id was:",
+    Loom.currentNetwork,
+    networkId
+  );
+  throw Error("Contract not deployed on DAppChain (network id error)");
+}
+
+Loom.instance = new Loom.web3.eth.Contract(
+  Loom.contract.abi,
+  Loom.currentNetwork.address,
+  {
+    from: Loom.currentUserAddress
   }
-
-  Loom.instance = new Loom.web3.eth.Contract(
-    Loom.contract.abi,
-    Loom.currentNetwork.address,
-    {
-      from: Loom.currentUserAddress
-    }
-  );
+);
 }
 
- async function getCurrentNetwork() {
-  return await Loom.web3.eth.net.getId();
+async function getCurrentNetwork() {
+return await Loom.web3.eth.net.getId();
 }
+
