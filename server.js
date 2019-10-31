68e17e82cbe2081cb5497226c6e1064e8b5d2545
lrypto <lloydrebello@gmail.com>
2019-10-31 13:38:29 -0400

    fixed formatting of server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const express         = require('express');
-const MongoClient     = require('mongodb').MongoClient;
-const bodyParser      = require('body-parser');
-
-const app             = express();
-
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
 const port  = 8000;
 
 require('./app/routes')(app, {});
